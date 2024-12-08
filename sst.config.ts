a9ee80dc9acc61cb500f918d40c9246fad9a3339
Maximilian Schön <maximilian@solenergikvalitet.se>
2024-12-08 19:47:23 +0100

    trying medium runners

--- sst.config.ts
+++ sst.config.ts
@@ -8,9 +8,27 @@ export default $config({
       home: "aws",
     };
   },
-  async run() {
-    const table = await import("./infra/db");
-    const bucket = await import("./infra/bucket");
-    const api = await import("./infra/api");
-  },
-});
+  console: {
+    autodeploy: {
+      target(event) {
+        if (event.type === "branch") {
+          return {
+            stage: event.branch
+              .replace(/[^a-zA-Z0-9-]/g, "-")
+              .replace(/-+/g, "-")
+              .replace(/^-/g, "")
+              .replace(/-$/g, ""),
+            runner: {
+              engine: "codebuild",
+              compute: "medium",
+              timeout: "1 hour",
+            },
+          };
+        }
+      },
+      async run() {
+        const table = await import("./infra/db");
+        const bucket = await import("./infra/bucket");
+        const api = await import("./infra/api");
+      },
+    });
